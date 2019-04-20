ca7fa568a323981974af8e218bcc768b17cbd64a
Mihai Nechifor <Mihai.Nechifor@endava.com>
2019-04-20 11:29:41 +0300

    week 2

---
+++ Homework1/ConsoleApp1/ConsoleApp1/CommonElementsOfArray.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    public class CommonElementsOfArray
+    {
+        //Find common elements between two arrays of integers. 
+        /// 1 4 5 7 82 3 5
+        /// 4 4 7 3 32 4 7 9
+
+        public static int[] Duplicates(int[] arr1, int[] arr2)
+        {
+            var count = FindNumberOfDuplicates(arr1, arr2);
+
+            int[] result = new int[count];
+            count = 0;
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                for (int j = 0; j < arr2.Length; j++)
+                {
+                    if (arr1[i] == arr2[j])
+                    {
+                        result[count++] = arr1[i];
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private static int FindNumberOfDuplicates(int[] arr1, int[] arr2)
+        {
+            int count = 0;
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                for (int j = 0; j < arr2.Length; j++)
+                {
+                    if (arr1[i] == arr2[j])
+                    {
+                        count++;
+                        break;
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
--- Homework1/ConsoleApp1/ConsoleApp1/Program.cs
+++ Homework1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Homework1
 {
@@ -7,30 +10,81 @@ namespace Homework1
     {
         static void Main(string[] args)
         {
-            int[] numbers = new int[] { 11, 32, 13, 4, 8, 55, 21, 88, 99 };
+            //OperatiiMatematice.Suma(3, 7);
 
-            //Console.WriteLine(numbers[0]);
-            //Console.WriteLine(numbers[1]);
-            //Console.WriteLine(numbers[2]);
-            //Console.WriteLine(numbers[3]);
-            //Console.WriteLine(numbers[4]);
-            //Console.WriteLine(numbers[5]);
-            //Console.WriteLine(numbers[6]);
+           //FindDuplicates();
 
-            Console.WriteLine("-----------");
+           // LinkedListExample();
 
-            for (int i = 0; i < numbers.Length; i++)
+
+           //DuplicatesInList();
+        }
+
+        private static void DuplicatesInList()
+        {
+//find duplicates in list
+            List<double> list = new List<double>() {10, 11, 50};
+
+            list.Add(10);
+            list.Add(20);
+            list.Add(11.3);
+            list.Add(20);
+
+            List<double> duplicate = new List<double>();
+
+            foreach (var element in list)
             {
-                Console.WriteLine(numbers[i]);
+                if (!duplicate.Contains(element))
+                {
+                    duplicate.Add(element);
+                }
+            }
+
+            foreach (var d in duplicate)
+            {
+                Console.Write(d + " ");
             }
         }
 
-        private static void TablaInmultirii()
+        private static void LinkedListExample()
+        {
+            LinkedList<int> list = new LinkedList<int>();
+
+            LinkedListNode<int> firstNode = new LinkedListNode<int>(10);
+
+            list.AddFirst(firstNode);
+
+            LinkedListNode<int> node2 = new LinkedListNode<int>(20);
+
+            list.AddAfter(firstNode, node2);
+
+
+            foreach (var i in list)
+            {
+            }
+        }
+
+        private static void FindDuplicates()
+        {
+            int[] arr1 = new int[7] {1, 4, 5, 7, 82, 3, 5};
+            int[] arr2 = new int[8] {4, 4, 7, 3, 32, 4, 7, 9};
+
+            int[] duplicates = CommonElementsOfArray.Duplicates(arr1, arr2);
+
+            foreach (var duplicate in duplicates)
+            {
+                Console.Write(duplicate + " ");
+            }
+
+           // int[]intersection = arr1.Intersect(arr2).ToArray();
+        }
+
+        public static void TablaInmultirii(int n)
         {
             Console.WriteLine("Enter the number:");
             int numar = Convert.ToInt32(Console.ReadLine());
 
-            for (int i = 0; i < 11; i++)
+            for (int i = 0; i < n; i++)
             {
                 Console.WriteLine(numar + " * " + i + " = " + numar * i);
 
@@ -39,6 +93,9 @@ namespace Homework1
                 Console.WriteLine($"{numar} * {i} = {numar * i}");
             }
         }
+
+
+
 
         private static void NumericalOperations()
         {
